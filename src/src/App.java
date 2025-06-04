57e10ff0a8b6cfc21e04b5f8e6706d19807153f8
karly <kalbarransanchez@danielcastelao.org>
2025-06-04 13:51:36 +0200

    inicio

--- src/src/App.java
+++ src/src/App.java
@@ -4,12 +4,12 @@
  */
 public class App {
     public static void main(String[] args) {
-        Controller.crearCoche("LaFerrari", "SBC 1234");
-        Controller.crearCoche("Alpine", "HYU 4567");
-        Controller.crearCoche("Aston Martin", "FGH 3333");
+        Controller.crearCoche("Ferrari", "SBC 1234");
+        Controller.crearCoche("aaa", "HYU 4567");
+        Controller.crearCoche("Ford", "FGH 3333");
 
         Controller.cambiarVelocidad("SBC 1234", 85);
-        Controller.ponerGasolina("SBC 1234", -45); // para activar alerta
+        Controller.ponerGasolina("SBC 1234", -50); // para activar la alerta
         Controller.avanzar("SBC 1234", 100); // consume gasolina y puede activar alerta
     }
 }
